e6560d1f2145e15f0af66b77bd5817c76ac86f0f
Britto <49052735+brittoatpierceinfotech@users.noreply.github.com>
2019-07-06 13:52:30 +0530

    Update hr_advance_payslip.py
    This fix consider only the advance salaries got during the payroll period install of all. The previous logic of checking only the month would result in inclusion of the advance salary in every consecutive years on the specified month. and also the datetime.strptime(date_from, '%Y-%m-%d') takes the parameter as string whereas now the parameters are date objects.

--- ohrms_salary_advance/models/hr_advance_payslip.py
+++ ohrms_salary_advance/models/hr_advance_payslip.py
@@ -12,15 +12,30 @@ class SalaryRuleInput(models.Model):
         res = super(SalaryRuleInput, self).get_inputs(contract_ids, date_from, date_to)
         contract_obj = self.env['hr.contract']
         emp_id = contract_obj.browse(contract_ids[0].id).employee_id
-        adv_salary = self.env['salary.advance'].search([('employee_id', '=', emp_id.id)])
+        
+        # Commented by Pierce Infotech - Start
+        # adv_salary = self.env['salary.advance'].search([('employee_id', '=', emp_id.id)])
+        # for adv_obj in adv_salary:
+        #     current_date = datetime.strptime(date_from, '%Y-%m-%d').date().month
+        #     date = adv_obj.date
+        #     existing_date = datetime.strptime(date, '%Y-%m-%d').date().month
+        #     if current_date == existing_date:
+        #         state = adv_obj.state
+        #         amount = adv_obj.advance
+        #         for result in res:
+        #             if state == 'approve' and amount != 0 and result.get('code') == 'SAR':
+        #                 result['amount'] = amount
+        # Commented by Pierce Infotech - End
+
+        # Fix by Pierce Infotech - Start
+        # This fix consider only the advance salaries got during the payroll period install of all. The previous logic of checking only the month would result in inclusion of the advance salary in every consecutive years on the specified month.
+        adv_salary = self.env['salary.advance'].search([('employee_id', '=', emp_id.id), ('date', '>=', date_from), ('date', '<=', date_to)])
         for adv_obj in adv_salary:
-            current_date = datetime.strptime(date_from, '%Y-%m-%d').date().month
-            date = adv_obj.date
-            existing_date = datetime.strptime(date, '%Y-%m-%d').date().month
-            if current_date == existing_date:
-                state = adv_obj.state
-                amount = adv_obj.advance
-                for result in res:
-                    if state == 'approve' and amount != 0 and result.get('code') == 'SAR':
-                        result['amount'] = amount
+            state = adv_obj.state
+            amount = adv_obj.advance
+            for result in res:
+                if state == 'approve' and amount != 0 and result.get('code') == 'SAR':
+                    result['amount'] = amount
+        # Fix by Pierce Infotech - End
+        
         return res
